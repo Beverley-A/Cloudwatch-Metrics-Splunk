5c73252415e5d61322b2c93b399d5a06b37291e3
Beverley-A <157363316+Beverley-A@users.noreply.github.com>
2024-01-22 10:47:51 +0000

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "splunk_observability_cloud_aws_account_id" {
+  description = "Account ID for Splunk Observability Cloud"
+  default     = ""
+}
+
+variable "splunk_observability_cloud_aws_external_id" {
+  description = "External ID for Splunk Observability Cloud"
+  default     = ""
+}
